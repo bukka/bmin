bf1c946b4f1d8a255eb23bf138eb568aea356deb
Jakub Zelenka <jakub.zelenka@gmail.com>
2010-03-14 15:33:45 +0100

    Relicensing to GPL version 3

--- kernel/quinemccluskey.h
+++ kernel/quinemccluskey.h
@@ -1,3 +1,23 @@
+/*
+ * quinemccluskey.h - Quine–McCluskey algorithm
+ * created date: 10/25/2009
+ *
+ * Copyright (C) 2009-2010 Jakub Zelenka.
+ *
+ * Bmin is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * Bmin is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Bmin; if not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef QUINEMCCLUSKEY_H
 #define QUINEMCCLUSKEY_H
 
--- qtgui/truthtablemodel.cpp
+++ qtgui/truthtablemodel.cpp
@@ -1,23 +1,21 @@
 /*
- *
+ * truthtablemodel.cpp - model for truth table
  * created date: 3/7/2010
  *
- * Copyright (C) 2009-2010 Jakub Zelenka.
+ * Copyright (C) 2010 Jakub Zelenka.
  *
  * Bmin is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public
  * License as published by the Free Software Foundation; either
- * version 2 of the License, or (at your option) any later version.
+ * version 3 of the License, or (at your option) any later version.
  *
  * Bmin is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * General Public License for more details.
  *
- * You should have received a copy of the GNU General Public
- * License along with Bmin; if not, write to the Free
- * Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
- * 02111-1307 USA.
+ * You should have received a copy of the GNU General Public License
+ * along with Bmin; if not, see <http://www.gnu.org/licenses/>.
  */
 
 #include "truthtablemodel.h"
